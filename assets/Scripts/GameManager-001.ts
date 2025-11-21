160aca4bcc998d9aa915ce424bc49659b9774a0d
Sanjay-101097 <134190907+Sanjay-101097@users.noreply.github.com>
2025-11-21 18:50:38 +0530

    audio added

--- assets/Scripts/GameManager-001.ts
+++ assets/Scripts/GameManager-001.ts
@@ -34,7 +34,7 @@ export class GameManager extends Component {
     particle: Node = null;
 
     @property(Node)
-    Canvas: Node = null;
+    CTA: Node = null;
 
     @property(Node)
     Canvas2: Node = null;
@@ -74,7 +74,7 @@ export class GameManager extends Component {
 
     collectorArr: Node[] = [];
     busArr: Node[] = [];
-    buscolor: string[] = ["55", "33", "11", "00", "51", "33", "06", "52"];
+    buscolor: string[] = ["55", "33", "11", "00", "15", "33", "77", "60", "52", "33", "22", "77","65"];
     currentBusidx = 0;
     colliderinfo: Vec2[] = [new Vec2(2.7, 5.6), new Vec2(2, 4.2), , new Vec2(1.3, 2.7)]
     colliderpos: number[] = [4.7, 3.4, 2, 0.6]
@@ -94,12 +94,12 @@ export class GameManager extends Component {
         this.audioSource = this.node.getComponent(AudioSource);
 
 
-        this.Canvas.active = true;
-        let nodeToAnimate = this.Canvas.getChildByName("Label");
+        // this.Canvas.active = true;
+        let nodeToAnimate = this.CTA.parent.getChildByName("lable");
         const zoomIn = tween(nodeToAnimate)
-            .to(0.8, { scale: v3(1.040, 1.040, 1.040) });
+            .to(0.5, { scale: v3(0.9, 0.9, 0.9) });
         const zoomOut = tween(nodeToAnimate)
-            .to(0.8, { scale: v3(1.039, 1.039, 1.039) });
+            .to(0.5, { scale: v3(1, 1, 1) });
         tween(nodeToAnimate)
             .sequence(zoomIn, zoomOut)
             .union()
@@ -114,15 +114,15 @@ export class GameManager extends Component {
     sethandpos() {
         const visibleSize = view.getVisibleSizeInPixel();
         const height = window.innerHeight;
-        let xdiff = 0;
-        let ydiff = 60;
+        let xdiff = 50;
+        let ydiff = -20;
 
         if (height >= 800) {
             xdiff = 0
             ydiff = 40
         }
 
-        let nodeToAnimate = this.Canvas.getChildByName("BubbleIdle")
+        let nodeToAnimate = this.CTA.parent.getChildByName("BubbleIdle")
         nodeToAnimate.setPosition(-228 + xdiff, -252 + ydiff)
         const change = tween(nodeToAnimate).delay(0.3)
             .call(() => {
@@ -137,7 +137,7 @@ export class GameManager extends Component {
         const Out = tween(nodeToAnimate)
             .to(0.8, { position: v3(-228 + xdiff, -252 + ydiff, 0) });
         tween(nodeToAnimate)
-            .sequence(change, In, change, Out)
+            .sequence(change, change)
             .union()
             .repeatForever()
             .start();
@@ -161,14 +161,15 @@ export class GameManager extends Component {
 
     private Bidx = 0;
     private Cidx = 0;
-
+    firsttouch = false;
     collectoranim: boolean = false;
 
     onTouchStart(event) {
-        // this.anim();
-        if (this.isAnimating) return; // Block touch during animations
-        this.isAnimating = true;
-        // this.Collector.getComponent(AudioSource).play();
+        if (!this.firsttouch) {
+            this.firsttouch = true;
+            this.Collector.getComponent(AudioSource).play();
+            // this.BG.play();
+        }
 
         // Tween.stopAll();
         const mousePos = event.getLocation();
@@ -180,7 +181,8 @@ export class GameManager extends Component {
         const maxDistance = 1000; // Maximum ray distance
         const queryTrigger = true; // Include trigger colliders
 
-        this.Canvas.active = false;
+        this.CTA.parent.getChildByName("BubbleIdle").active = false;
+        this.CTA.parent.getChildByName("lable").active = false;
         if (PhysicsSystem.instance.raycastClosest(ray, mask, maxDistance, queryTrigger)) {
 
             const result = PhysicsSystem.instance.raycastClosestResult;
@@ -189,7 +191,7 @@ export class GameManager extends Component {
 
 
             if (node.children.length > 1) {
-                // this.audioSource.playOneShot(this.Audioclips[0], 1);
+                this.audioSource.playOneShot(this.Audioclips[0], 1);
                 this.Cardmovement(node)
 
                 this.scheduleOnce(() => {
@@ -220,6 +222,7 @@ export class GameManager extends Component {
     fsthalfidx = 0
     Snthalfidx = 0
 
+
     Cardmovement(node) {
         let sIdx = 0;
         let curntbus = this.BusArr[this.currentBusidx]
@@ -239,6 +242,10 @@ export class GameManager extends Component {
                 this.Snthalfidx += 1;
                 this.Bix += 1
             } else {
+                if (this.Collectoridx > 30) {
+                    this.CTAcall()
+                    return;
+                }
                 node.children[i].getComponent(Box).parent = this.Collector.children[this.Collectoridx]
                 this.Collectoridx += 1
             }
@@ -254,7 +261,13 @@ export class GameManager extends Component {
         this.schedule(() => {
             ar[idx].getComponent(Box).anim2()
             idx += 1
+            this.audioSource.playOneShot(this.Audioclips[2], 1);
             if (this.Bix >= 10) {
+                this.scheduleOnce(() => {
+                    this.audioSource.playOneShot(this.Audioclips[1], 1);
+                this.BusArr[this.currentBusidx].getChildByName("bus").children[0].active = true
+                this.BusArr[this.currentBusidx].getChildByName("bus").children[0].getComponent(ParticleSystem).play()
+                }, 0.3)
                 this.Bix = 0
                 this.crtCnt += 1
                 this.scheduleOnce(() => {
@@ -269,15 +282,20 @@ export class GameManager extends Component {
                         this.Snthalfidx = 0
                         if (this.crntLevel === 1 && this.crtCnt === 2) {
                             this.crntLevel += 1
-                            this.currentBusidx =0
+                            this.currentBusidx = 0
                             tween(this.Levels[0]).to(0.1, { x: -5000 }).call(() => {
                                 tween(this.Levels[1]).to(0.3, { x: -11.4 }).start()
                                 this.setbusColor();
-                                tween(this.BusArr[this.currentBusidx]).delay(0.3).to(0.2, { position: buspos }).start()
+                                tween(this.BusArr[this.currentBusidx]).delay(0.3).to(0.2, { position: buspos }).call(() => {
+                                    this.checkCollector()
+                                }).start()
                             }).start()
                         }
                     }).start()
                     this.currentBusidx += 1;
+                    if (this.currentBusidx > 2) {
+                        this.currentBusidx = 0
+                    }
                     if (this.crntLevel === 2) {
                         this.setbusColor();
                     }
@@ -295,9 +313,25 @@ export class GameManager extends Component {
 
     }
 
-    resetbusslots(bus:Node){
-        bus.getChildByName("bus").setScale(1,1,1);
-        for(let i=0;i<bus.children.length-1;i++){
+    CTAcall(){
+        this.CTA.active = true;
+                    let icon = this.CTA.children[1];
+                    
+                    let playbutton = this.CTA.children[2];
+                    tween(icon).delay(0.2).to(0.3, { scale: v3(1, 1.2, 1) }, { easing: "quadIn" }).to(0.3, { scale: v3(1.2, 1, 1) }, { easing: "quadIn" }).to(0.3, { scale: v3(1, 1, 1) }, { easing: "quadIn" }).start()
+                    tween(playbutton)
+                        .repeatForever(
+                            tween()
+                                .to(0.6, { scale: new Vec3(1.1, 1.1, 1) }, { easing: 'sineInOut' })
+                                .to(0.6, { scale: new Vec3(1.0, 1.0, 1) }, { easing: 'sineInOut' })
+                        )
+                        .start();
+                    this.Canvas2.active = false;
+    }
+
+    resetbusslots(bus: Node) {
+        bus.getChildByName("bus").setScale(1, 1, 1);
+        for (let i = 0; i < bus.children.length - 1; i++) {
             bus.children[i].children[0].destroy()
         }
     }
@@ -308,8 +342,8 @@ export class GameManager extends Component {
         let color = Number(this.buscolor[this.crtCnt - 2])
         let material1 = this.colorMaterials[Math.floor(color / 10)]
         let material2 = this.colorMaterials[color % 10]
-        bus.getChildByName("bus").getComponent(MeshRenderer).setMaterial(material1, 0);
-        bus.getChildByName("bus").getComponent(MeshRenderer).setMaterial(material2, 1);
+        bus.getChildByName("bus").getComponent(MeshRenderer).setMaterial(material1, 1);
+        bus.getChildByName("bus").getComponent(MeshRenderer).setMaterial(material2, 0);
     }
 
     checkCollector() {
@@ -320,6 +354,7 @@ export class GameManager extends Component {
         let len = this.Collectoridx
         for (let i = 0; i < len; i++) {
             let node = this.Collector.children[i]
+            if (!this.Collector.children[0].children[0]) return
             if (Number(node.children[0]?.name) === Math.floor(bus / 10) && this.fsthalfidx < 5) {
 
                 node.children[0].getComponent(Box).parent = curntbus.children[this.fsthalfidx]
@@ -334,14 +369,24 @@ export class GameManager extends Component {
                 this.Collectoridx -= 1
                 ar.push(node.children[0])
             }
+
         }
 
+        this.scheduleOnce(() => {
+            this.resetCollector()
+        }, 1)
         let idx = 0
 
         this.schedule(() => {
+            this.audioSource.playOneShot(this.Audioclips[2], 1);
             ar[idx].getComponent(Box).anim2()
             idx += 1
             if (this.Bix >= 10) {
+                this.scheduleOnce(() => {
+                    this.audioSource.playOneShot(this.Audioclips[1], 1);
+                this.BusArr[this.currentBusidx].getChildByName("bus").children[0].active = true
+                this.BusArr[this.currentBusidx].getChildByName("bus").children[0].getComponent(ParticleSystem).play()
+                }, 0.3)
                 this.Bix = 0
                 this.crtCnt += 1
                 this.scheduleOnce(() => {
@@ -355,20 +400,22 @@ export class GameManager extends Component {
                         this.Snthalfidx = 0
                         if (this.crntLevel === 1 && this.crtCnt === 2) {
                             this.crntLevel += 1
-                            this.currentBusidx =0
+                            this.currentBusidx = 0
                             tween(this.Levels[0]).to(0.1, { x: -5000 }).call(() => {
                                 this.setbusColor();
                                 tween(this.Levels[1]).to(0.1, { x: -11.4 }).start()
-                                tween(this.BusArr[this.currentBusidx]).delay(0.3).to(0.2, { position: buspos }).start()
+                                tween(this.BusArr[this.currentBusidx]).delay(0.3).to(0.2, { position: buspos }).call(() => {
+                                    this.checkCollector()
+                                }).start()
                             }).start()
                         }
                     }).start()
-                    
+
                     this.currentBusidx += 1;
-                    if( this.currentBusidx>2){
-                        this.currentBusidx =0
+                    if (this.currentBusidx > 2) {
+                        this.currentBusidx = 0
                     }
-                     if (this.crntLevel === 2) {
+                    if (this.crntLevel === 2) {
                         this.setbusColor();
                     }
                     if ((this.crntLevel === 1 && this.currentBusidx < 2) || this.crntLevel === 2)
@@ -381,6 +428,37 @@ export class GameManager extends Component {
         }, 0.06, ar.length - 1)
 
 
+    }
+
+    resetCollector() {
+        const collector = this.Collector;
+
+        // 1. Gather all cards from all boxes
+        const cards: Node[] = [];
+
+        for (let box of collector.children) {
+            const card = box.children[0];
+            if (card) cards.push(card);
+        }
+
+        // 2. Remove all cards from their boxes
+        for (let box of collector.children) {
+            const card = box.children[0];
+            if (card) card.removeFromParent();
+        }
+
+        // 3. Reassign cards to boxes in order (one card per box)
+        let i = 0;
+        for (let box of collector.children) {
+            if (i >= cards.length) break;
+
+            const card = cards[i];
+            box.addChild(card);
+            tween(card).to(0.1, { position: v3(0, 0, 0) }).start()
+            // card.setPosition(0, 0, 0);   // place card at center of box
+
+            i++;
+        }
     }
 
     playBeforeAnimation(node: Node, onComplete: () => void) {
@@ -620,16 +698,11 @@ export class GameManager extends Component {
     private dt = 0;
 
     update(deltaTime: number) {
-        if (this.enable) {
+        if (this.firsttouch) {
             this.dt += deltaTime;
-            if (this.dt >= 28 || this.wrongCnt >= 15) {
-                this.Canvas.active = true;
-                this.Canvas2.active = false;
-                this.Canvas.children[1].active = false;
-                this.Canvas.children[2].active = true;
-                this.Canvas.children[3].active = true;
-                this.Canvas.children[4].active = true;
-                this.Canvas.getChildByName("Label").active = false;
+            if (this.dt >= 50 ) {
+                this.CTAcall()
+                this.firsttouch = false
 
             }
         }
